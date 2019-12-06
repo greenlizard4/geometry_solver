0d398574107c92a13d5faa0cf368b9b1bed7a796
Ivan Hernandez <ivanh3@uci.edu>
2019-12-05 20:01:42 -0800

    Added my stuff

--- geometry_solver.py
+++ geometry_solver.py
@@ -8,6 +8,7 @@ areas = ['ar1','ar2','ar3','ar4','ar5','ar6','ar7','ar8','ar9']
 angles = ['a1','a2','a3','a4','a5','a6','a7','a8','a9','b1','b2','b3','b4','b5','b6','c1','c2','c3','c4','c5','c6','d1','d2','d3','d4','d5','d6']
 
 dictionary = {}
+null_flag = False
 
 def add_to_dictionary(key, name1, name2, value='NULL'):
     dictionary.setdefault(key,[])
@@ -19,442 +20,255 @@ def add_to_dictionary(key, name1, name2, value='NULL'):
     dictionary[key] = newList
 
 def check_dictionary(key, name1, name2, value='NULL'):
-    # if exists return true
-    # else return null/false
-
+    dictionary.setdefault(key,[])
+    if(value == 'NULL'):
+        return [name1,name2] in dictionary[key] or [name2,name1] in dictionary[key]
+    if(key == 'sum_value'):
+        return [name1,name2,value] in dictionary[key] or [name2,name1,value] in dictionary[key]
+    return [name1,name2,value] in dictionary[key]
+    
 #Predicates
 
 def set_parallel(name1, name2):
+    add_to_dictionary('parallel', name1, name2)
+    
     if(name1 in ['ls1','ls2','ls3','ls10']):
         if(name2 in ['ls1','ls2','ls3','ls10']):
-            #do nothing
-            return
-        elif(name2 in ['ls4','ls5','ls6','ls11']):
-            #set a5, ar1, ar2, ar3, ar7, ar8 to zero
-            #   also a1 if ls1 > 0
-            return
-        elif(name2 in ['ls7','ls8','ls9','ls12']):
-            #set a1, ar1, ar2, ar3, ar7, ar8 to zero
-            #   also a5 if ls3 > 0
             return
         else:
             #NULL
+            null_flag = True
             return
     elif(name1 in ['ls4','ls5','ls6','ls11']):
-        if(name2 in ['ls1','ls2','ls3','ls10']):
-            #set a5, ar1, ar2, ar3, ar7, ar8 to zero
-            #   also a1 if ls1 > 0
-            return
-        elif(name2 in ['ls4','ls5','ls6','ls11']):
-            #do nothing
-            return
-        elif(name2 in ['ls7','ls8','ls9','ls12']):
-            #set a9, ar1, ar2, ar3, ar7, ar8 to zero
-            #   also a1 if ls9 > 0
+        if(name2 in ['ls4','ls5','ls6','ls11']):
             return
         else:
             #NULL
+            null_flag = True
             return
-    elif(name1 in ['ls7','ls8','ls9','ls12']):
-        if(name2 in ['ls1','ls2','ls3','ls10']):
-            #set a1, ar1, ar2, ar3, ar7, ar8 to zero
-            #   also a5 if ls3 > 0
-            return
-        elif(name2 in ['ls4','ls5','ls6','ls11']):
-            #set a9, ar1, ar2, ar3, ar7, ar8 to zero
-            #   also a1 if ls9 > 0
-            return
-        elif(name2 in ['ls7','ls8','ls9','ls12']):
+    elif(name1 in ['ls7','ls8','ls9','ls12','ls4','ls5','ls6','ls11']):
+        if(name2 in ['ls7','ls8','ls9','ls12']):
             #do nothing
             return
         else:
             #NULL
+            null_flag = True
             return
     else:
-        #NULL? or make circle a line -> triangle a line
+        #NULL
         return
     return
 
 def set_perpendicular(name1, name2):
+    add_to_dictionary('perpendicular', name1, name2)
+
     if(name1 in ['ls1','ls2','ls3','ls10']):
         if(name2 in ['ls1','ls2','ls3','ls10']):
             #NULL
+            null_flag = True
             return
         elif(name2 in ['ls4','ls5','ls6','ls11']):
-            #
+            know_call(name1,name2)
             return
         elif(name2 in ['ls7','ls8','ls9','ls12']):
-            #
+            know_call(name1,name2)
             return
         else:
             #NULL
+            null_flag = True
             return
     elif(name1 in ['ls4','ls5','ls6','ls11']):
         if(name2 in ['ls1','ls2','ls3','ls10']):
-            #
+            know_call(name1,name2)
             return
         elif(name2 in ['ls4','ls5','ls6','ls11']):
             #NULL
+            null_flag = True
             return
         elif(name2 in ['ls7','ls8','ls9','ls12']):
-            #
+            know_call(name1,name2)
             return
         else:
             #NULL
+            null_flag = True
             return
     elif(name1 in ['ls7','ls8','ls9','ls12']):
         if(name2 in ['ls1','ls2','ls3','ls10']):
-            #
+            know_call(name1,name2)
             return
         elif(name2 in ['ls4','ls5','ls6','ls11']):
-            #
-            return
-        elif(name2 in ['ls7','ls8','ls9','ls12']):
-            #NULL
+            know_call(name1,name2)
             return
         else:
             #NULL
+            null_flag = True
             return
     return
 
 def set_equal(name1, name2):
+    add_to_dictionary('equal', name1, name2)
     if(name1 == 'ls1'):
         if(name2 == 'ls9'):
-            #a2 == a3, a2 == b1, a3 == b6
+            know_call(name1,name2)
             return
     elif(name1 == 'ls2'):
         if(name2 == 'ls5'):
-            #as2 == as4
-            #check if ls8 is equal
+            know_call(name1,name2)
             return
         if(name2 == 'ls8'):
-            #as2 == as6
-            #check if ls5 is equal
+            know_call(name1,name2)
             return
     elif(name1 == 'ls3'):
         if(name2 == 'ls4'):
-            #a4 == a6
+            know_call(name1,name2)
             return
         return
     elif(name1 == 'ls4'):
         if(name2 == 'ls3'):
-            #a4 == a6
+            know_call(name1,name2)
             return
         return
     elif(name1 == 'ls5'):
         if(name2 == 'ls2'):
-            #as4 == as2
-            #check if ls5 is equal
+            know_call(name1,name2)
             return
         if(name2 == 'ls8'):
-            #as4 == as6
-            #check if ls2 is equal
+            know_call(name1,name2)
             return
         return
     elif(name1 == 'ls6'):
         if(name2 == 'ls7'):
-            #a7 == a8
+            know_call(name1,name2)
             return
         return
     elif(name1 == 'ls7'):
         if(name2 == 'ls6'):
-            #a7 == a8
+            know_call(name1,name2)
             return
         return
     elif(name1 == 'ls8'):
         if(name2 == 'ls2'):
-            #as6 == as2
-            #check if ls5 is equal
+            know_call(name1,name2)
             return
         if(name2 == 'ls5'):
-            #as6 == as4
-            #check if ls2 is equal
+            know_call(name1,name2)
             return
         return
     elif(name1 == 'ls9'):
         if(name2 == 'ls1'):
-            #a2 == a3
+            know_call(name1,name2)
             return
         return
     elif(name1 == 'ls10'):
         if(name2 == 'ls11'):
-            #a1 == a9
+            know_call(name1,name2)
             return
         if(name2 == 'ls12'):
-            #a5 == a9
+            know_call(name1,name2)
             return
         return
     elif(name1 == 'ls11'):
         if(name2 == 'ls10'):
-            #a1 == a9
+            know_call(name1,name2)
             return
         if(name2 == 'ls12'):
-            #a1 == a5
+            know_call(name1,name2)
             return
         return
     elif(name1 == 'ls12'):
         if(name2 == 'ls10'):
-            #a5 == a9
+            know_call(name1,name2)
             return
         if(name2 == 'ls11'):
-            #a1 == a9
+            know_call(name1,name2)
             return
         return
     #ARCS
     elif(name1 == 'as1'):
-        if(name2 == 'as3'):
-            #a1 == a5 ?
-            return
-        if(name2 == 'as5'):
-            #a1 == a9 ?
-            return
         return
     elif(name1 == 'as2'):
         if(name2 == 'as4'):
-            #ls2 == ls5
-            #ar4 == ar5
+            know_call(name1,name2)
             return
         if(name2 == 'as6'):
-            #ls2 == ls8
-            #ar4 == ar6
+            know_call(name1,name2)
             return
         return
     elif(name1 == 'as3'):
-        if(name2 == 'as1'):
-            #a5 == a1 ?
-            return
-        if(name2 == 'as5'):
-            #a5 == a9 ?
-            return
         return
     elif(name1 == 'as4'):
         if(name2 == 'as2'):
-            #ls5 == ls2
-            #ar5 == ar4
+            know_call(name1,name2)
             return
         if(name2 == 'as6'):
-            #ls5 == ls8
-            #ar5 == ar6
+            know_call(name1,name2)
             return
         return
     elif(name1 == 'as5'):
-        if(name2 == 'as1'):
-            #a9 == a1 ?
-            return
-        if(name2 == 'as3'):
-            #a9 == a5 ?
-            return
+        know_call(name1,name2)
         return
     elif(name1 == 'as6'):
         if(name2 == 'as2'):
-            #ls8 == ls2
-            #ar6 == ar4
+            know_call(name1,name2)
             return
         if(name2 == 'as4'):
-            #ls8 == ls5
-            #ar6 == ar5
+            know_call(name1,name2)
             return
         return
     elif(name1 == 'as7'):
-        #?
+        #NULL
+        null_flag = True
         return
     #AREAS
-    elif(name1 == 'ar1'):
-        return
-    elif(name1 == 'ar2'):
-        return
-    elif(name1 == 'ar3'):
-        return
-    elif(name1 == 'ar4'):
-        if(name2 == 'ar5'):
-            #check ls3 == ls4 or a4 == a6
-            #   ar4 is congruent to ar5
+    elif(name1 in ['ar8','ar9'] or name2 in ['ar8','ar9']):
+        if(name1 != name2 and name1 in ['ar8','ar9'] and name2 in ['ar8','ar9']):
+            know_call(name1,name2)
             return
-        elif(name2 == 'ar6'):
-            #check ls1 == ls9 or a2 == a3
-            #   ar4 is congruent to ar6
-            return
+        null_flag = True
         return
-    elif(name1 == 'ar5'):
-        return
-    elif(name1 == 'ar6'):
-        return
-    elif(name1 == 'ar7'):
-        return
-    elif(name1 == 'ar8'):
-        return
+    elif(name1 in areas and name2 in areas):
+        know_call(name1,name2)
     #ANGLES
-    elif(name1 == 'a1'):
-        if(name2 == 'a5'):
-            #ls11 == ls12
-            return
-        elif(name2 == 'a9'):
-            #ls10 == ls12
-            return
-        return
-    elif(name1 == 'a2'):
-        if(name2 == 'a3'):
-            #ls1 == ls9
-            return
-        return
-    elif(name1 == 'a3'):
-        if(name2 == 'a2'):
-            #ls1 == ls9
-            return
-        return
-    elif(name1 == 'a4'):
-        if(name2 == 'a6'):
-            #ls3 == ls4
-            return
-        return
-    elif(name1 == 'a5'):
-        if(name2 == 'a1'):
-            #ls11 == ls12
-            return
-        elif(name2 == 'a9'):
-            #ls10 == ls12
-            return
-        return
-    elif(name1 == 'a6'):
-        if(name2 == 'a4'):
-            #ls3 == ls4
-            return
-        return
-    elif(name1 == 'a7'):
-        if(name2 == 'a8'):
-            #ls6 == ls7
-            return
-        return
-    elif(name1 == 'a8'):
-        if(name2 == 'a7'):
-            #ls6 == ls7
-            return
-        return
-    elif(name1 == 'a9'):
-        if(name2 == 'a1'):
-            #ls10 == ls11
-            return
-        elif(name2 == 'a5'):
-            #ls10 == ls12
-            return
-        return
-    elif(name1 == 'b1'):
-        if(name2 == 'c1' or name2 == 'd1'):
-            #b1 = c1 = d1 = a2 = 90
-            #set_sum_value_value(b1, c1, 180)
-            #set_sum_value_value(b1, d1, 180)
-            return
-        return
-    elif(name1 == 'b2'):
-        if(name2 == 'c2' or name2 == 'd2'):
-            #b2 = c2 = d2 = a4 = 90
-            #set_sum_value_value(b2, c2, 180)
-            #set_sum_value_value(b2, d2, 180)
-            return
-        return
-    elif(name1 == 'b3'):
-        if(name2 == 'c3' or name2 == 'd3'):
-            #b3 = c3 = d3 = a6 = 90
-            #set_sum_value_value(b3, c3, 180)
-            #set_sum_value_value(b3, d3, 180)
-            return
-        return
-    elif(name1 == 'b4'):
-        if(name2 == 'c4' or name2 == 'd4'):
-            #b4 = c4 = d4 = a8 = 90
-            #set_sum_value_value(b4, c4, 180)
-            #set_sum_value_value(b4, d4, 180)
-            return
-        return
-    elif(name1 == 'b5'):
-        if(name2 == 'c5' or name2 == 'd5'):
-            #b5 = c5 = d5 = a7 = 90
-            #set_sum_value_value(b5, c5, 180)
-            #set_sum_value_value(b5, d5, 180)
-            return
-        return
-    elif(name1 == 'b6'):
-        if(name2 == 'c6' or name2 == 'd6'):
-            #b6 = c6 = d6 = a3 = 90
-            #set_sum_value_value(b6, c6, 180)
-            #set_sum_value_value(b6, d6, 180)
-            return
-        return
-    elif(name1 == 'c1'):
-        if(name2 == 'b1' or name2 == 'a2'):
-            #b1 = c1 = d1 = a2 = 90
-            return
-        return
-    elif(name1 == 'c2'):
-        if(name2 == 'b2' or name2 == 'a4'):
-            #b2 = c2 = d2 = a4 = 90
-            return
-        return
-    elif(name1 == 'c3'):
-        if(name2 == 'b3' or name2 == 'a6'):
-            #b3 = c3 = d3 = a6 = 90
-            return
-        return
-    elif(name1 == 'c4'):
-        if(name2 == 'b4' or name2 == 'a8'):
-            #b4 = c4 = d4 = a8 = 90
-            return
-        return
-    elif(name1 == 'c5'):
-        if(name2 == 'b5' or name2 == 'a7'):
-            #b5 = c5 = d5 = a7 = 90
-            return
-        return
-    elif(name1 == 'c6'):
-        if(name2 == 'b6' or name2 == 'a3'):
-            #b6 = c6 = d6 = a3 = 90
-            return
-        return
-    elif(name1 == 'd1'):
-        if(name2 == 'b1' or name2 == 'a2'):
-            #b1 = c1 = d1 = a2 = 90
-            return
-        return
-    elif(name1 == 'd2'):
-        if(name2 == 'b2' or name2 == 'a4'):
-            #b2 = c2 = d2 = a4 = 90
-            return
-        return
-    elif(name1 == 'd3'):
-        if(name2 == 'b3' or name2 == 'a6'):
-            #b3 = c3 = d3 = a6 = 90
-            return
-        return
-    elif(name1 == 'd4'):
-        if(name2 == 'b4' or name2 == 'a8'):
-            #b4 = c4 = d4 = a8 = 90
-            return
-        return
-    elif(name1 == 'd5'):
-        if(name2 == 'b5' or name2 == 'a7'):
-            #b5 = c5 = d5 = a7 = 90
-            return
-        return
-    elif(name1 == 'd6'):
-        if(name2 == 'b6' or name2 == 'a3'):
-            #b6 = c6 = d6 = a3 = 90
-            return
+    elif(name1 in angles and name2 in angles):
+        know_call(name1,name2)
+    else:
+        null_flag = True
         return
 
 def set_fraction(name1, name2, fraction):
+    add_to_dictionary('fraction', name1, name2,fraction)
+    know_call(name1,name2)
     return
 
 def set_sum_value(name1, name2, sum_value):
+    add_to_dictionary('sum_value', name1, name2, sum_value)
+    if(name1 in angles and name2 in angles and sum_value > 360):
+        #NULL
+        null_flag = True
+        return
+        
+    if(sum_value == 0):
+        #NULL
+        null_flag = True
+        return
+    
+    know_call(name1,name2)
     return
 
 def set_tan(name1, name2):
+    #NULL
+    null_flag = True
     return
 
 #NOT INPUTS
 def set_similar(name1, name2):
+    add_to_dictionary('similar',name1,name2)
+    know_call(name1,name2)
     return
 
 def set_congruent(name1, name2):
+    add_to_dictionary('congruent',name1,name2)
+    know_call(name1,name2)
     return
 
 #Knowables
@@ -782,9 +596,9 @@ def know_ls7():
     if check_dictionary("equal", "a7", "a8") == True and check_dictionary("equal", "ls6", "ls7") == False:
         add_to_dictionary("equal", "ls6", "ls7");
         know_ls6();
-    return
+        return
 
-if check_dictionary("perpendicular", "ls10", "ls12") == False and check_dictionary("perpendicular", "ls7", "ls10"):
+    if check_dictionary("perpendicular", "ls10", "ls12") == False and check_dictionary("perpendicular", "ls7", "ls10"):
         add_to_dictionary("perpendicular", "ls10", "ls12");
         know_ls10();
     if check_dictionary("perpendicular", "ls11", "ls12") == False and check_dictionary("perpendicular", "ls7", "ls11"):
@@ -1401,8 +1215,125 @@ def know_ar8():
 def know_ar9():
     return
 
+def know_call(name1, name2):
+    if(name1 == 'ls1' or name2 == 'ls1'):
+        know_ls1()
+    if(name1 == 'ls2' or name2 == 'ls2'):
+        know_ls2()
+    if(name1 == 'ls3' or name2 == 'ls3'):
+        know_ls3()
+    if(name1 == 'ls4' or name2 == 'ls4'):
+        know_ls4()
+    if(name1 == 'ls5' or name2 == 'ls5'):
+        know_ls5()
+    if(name1 == 'ls6' or name2 == 'ls6'):
+        know_ls6()
+    if(name1 == 'ls7' or name2 == 'ls7'):
+        know_ls7()
+    if(name1 == 'ls8' or name2 == 'ls8'):
+        know_ls8()
+    if(name1 == 'ls9' or name2 == 'ls9'):
+        know_ls9()
+    if(name1 == 'ls10' or name2 == 'ls10'):
+        know_ls10()
+    if(name1 == 'ls11' or name2 == 'ls11'):
+        know_ls11()
+    if(name1 == 'ls12' or name2 == 'ls12'):
+        know_ls12()
+    #ARCS
+    if(name1 == 'as1' or name2 == 'as1'):
+        know_as1()
+    if(name1 == 'as2' or name2 == 'as2'):
+        know_as2()
+    if(name1 == 'as3' or name2 == 'as3'):
+        know_as3()
+    if(name1 == 'as4' or name2 == 'as4'):
+        know_as4()
+    if(name1 == 'as5' or name2 == 'as5'):
+        know_as5()
+    if(name1 == 'as6' or name2 == 'as6'):
+        know_as6()
+    if(name1 == 'as7' or name2 == 'as7'):
+        know_as7()
+        
+    #ANGLES
+    if(name1 == 'a1' or name2 == 'a1'):
+        know_a1()
+    if(name1 == 'a2' or name2 == 'a2'):
+        know_a2()
+    if(name1 == 'a3' or name2 == 'a3'):
+        know_a3()
+    if(name1 == 'a4' or name2 == 'a4'):
+        know_a4()
+    if(name1 == 'a5' or name2 == 'a5'):
+        know_a5()
+    if(name1 == 'a6' or name2 == 'a6'):
+        know_a6()
+    if(name1 == 'a7' or name2 == 'a7'):
+        know_a7()
+    if(name1 == 'a8' or name2 == 'a8'):
+        know_a8()
+    if(name1 == 'a9' or name2 == 'a9'):
+        know_a9()
+    if(name1 == 'b1' or name2 == 'b1'):
+        know_b1()
+    if(name1 == 'b2' or name2 == 'b2'):
+        know_b2()
+    if(name1 == 'b3' or name2 == 'b3'):
+        know_b3()
+    if(name1 == 'b4' or name2 == 'b4'):
+        know_b4()
+    if(name1 == 'b5' or name2 == 'b5'):
+        know_b5()
+    if(name1 == 'b6' or name2 == 'b6'):
+        know_b6()
+    if(name1 == 'c1' or name2 == 'c1'):
+        know_c1()
+    if(name1 == 'c2' or name2 == 'c2'):
+        know_c2()
+    if(name1 == 'c3' or name2 == 'c3'):
+        know_c3()
+    if(name1 == 'c4' or name2 == 'c4'):
+        know_c4()
+    if(name1 == 'c5' or name2 == 'c5'):
+        know_c5()
+    if(name1 == 'c6' or name2 == 'c6'):
+        know_c6()
+    if(name1 == 'd1' or name2 == 'd1'):
+        know_d1()
+    if(name1 == 'd2' or name2 == 'd2'):
+        know_d2()
+    if(name1 == 'd3' or name2 == 'd3'):
+        know_d3()
+    if(name1 == 'd4' or name2 == 'd4'):
+        know_d4()
+    if(name1 == 'd5' or name2 == 'd5'):
+        know_d5()
+    if(name1 == 'd6' or name2 == 'd6'):
+        know_d6()
+        
+    #AREAS
+    if(name1 == 'ar1' or name2 == 'ar1'):
+        know_ar1()
+    if(name1 == 'ar2' or name2 == 'ar2'):
+        know_ar2()
+    if(name1 == 'ar3' or name2 == 'ar3'):
+        know_ar3()
+    if(name1 == 'ar4' or name2 == 'ar4'):
+        know_ar4()
+    if(name1 == 'ar5' or name2 == 'ar5'):
+        know_ar5()
+    if(name1 == 'ar6' or name2 == 'ar6'):
+        know_ar6()
+    if(name1 == 'ar7' or name2 == 'ar7'):
+        know_ar7()
+    if(name1 == 'ar8' or name2 == 'ar8'):
+        know_ar8()
+    if(name1 == 'ar9' or name2 == 'ar9'):
+        know_ar9()
 #output
 
 def get_all():
-    dictionary = {'equal':[['ls1','ls2'],['ls1','ls3']]}
+    if(null_flag):
+        return 'null'
     return dictionary
